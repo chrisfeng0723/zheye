6c8d1d3c5d34ffd6d9788dc6fc31413e7c4eef55
fengxinlei <fengxinlei@360.cn>
2021-05-23 18:40:15 +0800

    columnuList组件

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,48 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+  <div class="container">
+    <ColumnList :list="list"></ColumnList>
+  </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue'
-import HelloWorld from './components/HelloWorld.vue'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import ColumnList, { ColumnProps } from './components/ColumnList.vue'
+
+const testData: ColumnProps[] = [
+  {
+    id: 1,
+    title: 'tetst1的专栏',
+    description: '这是一个有意思的专栏',
+    avatar:
+      'https://p6.bdxiguaimg.com/img/mosaic-legacy/ff82000018d9a545f5cb~tplv-xg-center-qs:88:88:q75.webp'
+  },
+  {
+    id: 2,
+    title: 'tetst2的专栏',
+    description: '这是一个有意思的专栏',
+    avatar:
+      'https://p6.bdxiguaimg.com/img/mosaic-legacy/ff82000018d9a545f5cb~tplv-xg-center-qs:88:88:q75.webp'
+  },
+  {
+    id: 3,
+    title: 'tetst3  的专栏',
+    description: '这是一个有意思的专栏',
+    avatar:
+      'https://p6.bdxiguaimg.com/img/mosaic-legacy/ff82000018d9a545f5cb~tplv-xg-center-qs:88:88:q75.webp'
+  }
+]
 
 export default defineComponent({
   name: 'App',
   components: {
-    HelloWorld
+    ColumnList
+  },
+
+  setup () {
+    return {
+      list: testData
+    }
   }
 })
 </script>
