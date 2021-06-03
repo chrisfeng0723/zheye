eefaced7c66cbcac2ee9ba841b25796529791103
fengxinlei <fengxinlei@360.cn>
2021-06-03 18:31:49 +0800

    add整理数据l

---
+++ src/store/type.ts
@@ -0,0 +1,77 @@
+export interface UserProps {
+    isLogin: boolean,
+    nickName: string,
+    _id?: string,
+    column?: string,
+    email?: string,
+    description?: string,
+    avatar?: string,
+}
+
+export interface ImageProps {
+    _id?: string,
+    url?: string,
+    fitUrl?: string,
+    createdAt?: string,
+}
+
+export interface ColumnProps{
+    _id?:string,
+    url?:string,
+    fitUrl?:string,
+    createdAt?:string,
+}
+
+export interface PostProps{
+    _id?:string,
+    title:string,
+    excerpt?:string,
+    content?:string,
+    image?:string|ImageProps,
+    column:string,
+    author?:string|UserProps,
+    createdAt?:string,
+    isHTML?:boolean,
+
+}
+
+export interface RuleProps {
+    type:'required'| 'email' |'custom',
+    message:string,
+    validator?:()=>boolean,
+}
+
+export type MessageType = 'success'|'error'|'default'
+
+export interface ResponseType<p={}>{
+    code:number,
+    msg:string,
+    data:p,
+}
+
+export interface GloabalErrorProps{
+    status: boolean,
+    message:string,
+
+}
+
+interface listProps<p> {
+    [id:string]:p,
+}
+
+export interface LoadedPostProps{
+    columnId?:string,
+    currentPage?:number,
+    total?:number,
+}
+
+export interface GloadDataProps{
+    token:string,
+    error:GloabalErrorProps,
+    loading:boolean,
+    columns:{
+        data: listProps<ColumnProps>,
+        loadedColumns:listProps<LoadedPostProps>,
+    },
+    user:UserProps,
+}
